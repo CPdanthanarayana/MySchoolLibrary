b1c4b63f59223f9309e9a8636e3504decb2b1e5c
MadaraGallage <164727995+MadaraGallage@users.noreply.github.com>
2024-06-14 13:55:15 +0530

    Barcode files upload

---
+++ Gechanneling/message.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+
+    <link rel="stylesheet" href="message.css" />
+  </head>
+  <body>
+    <div class="conteiner">
+      <!-- navbar -->
+      <div class="navbar">
+        <img src="logo.png" class="logo" />
+        <h1 class="logo-name">GovernmentEchanneling</h1>
+        <nav>
+          <ul>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="">Support</a></li>
+            <li><a href="">About</a></li>
+          </ul>
+        </nav>
+      </div>
+      <!-- body -->
+       <div class="set">
+        <div class="image">
+          <img src="available.png" alt="">
+        </div>
+        <div class="name">
+          <h1>SUCCESSFUL !</h1>
+        </div>
+       </div>
+
+      <!-- footer -->
+      <footer class="footer">
+        <div class="footer-conteiner">
+          <div class="fcol1">
+            <ul>
+              <div class="logo-container">
+                <img src="logo.png" class="logo2" />
+                <h1 class="logo-name">GovernmentEchanneling</h1>
+              </div>
+              <br />
+              <li>Email: info@govchannel.lk</li>
+              <li>Phone: +94 11 123 4567</li>
+            </ul>
+          </div>
+          <div class="fcol2">
+            <h1>Other</h1>
+            <p>Privacy Policy</p>
+            <p>Terms Of Service</p>
+          </div>
+        </div>
+        <div class="fcol3">
+          <nav>
+            <ul>
+              <li><a href="home.html">Home</a></li>
+              <li><a href="">Support</a></li>
+              <li><a href="">About</a></li>
+            </ul>
+          </nav>
+        </div>
+        <hr class="hr">
+        <div class="end">
+          <h5>© 2024 Government e-Channeling System. All rights reserved.</h5>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
